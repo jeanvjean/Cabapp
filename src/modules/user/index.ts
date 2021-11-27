eeb2d9b79e547e94ba3d63e36acbad04eef1bb7a
jeanvaljean <fjohn087@gmail.com>
2021-11-27 17:39:20 +0100

    updated expires in

--- src/modules/user/index.ts
+++ src/modules/user/index.ts
@@ -511,7 +511,7 @@ class User extends Module {
         id: user._id.toString(),
         email:user.email.toString()
       }
-      let expiresIn = 180 //1000 * 60 * 60 * 24
+      let expiresIn = 1000 * 60 * 60 * 24
       let token = sign(payload, signTokenKey, {expiresIn});
       await createLog({
         user:user._id,
